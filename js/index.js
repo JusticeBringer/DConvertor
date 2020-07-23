d06c8d492870f7852031f1e86f02a404ca1cb954
justicebringer <gabithebigg@gmail.com>
2020-07-23 20:49:20 +0300

    Added design

--- js/index.js
+++ js/index.js
@@ -1,27 +1,23 @@
-window.onload = function (){
-
-	var proxyUrl = 'https://justicebringer.github.io/DConvertor/';
-    var targetUrl = 'https://www.poeziitraiandorz.ro';
-	
-
+function forImportIo(){
+	var proxyUrl = 'https://justicebringer.github.io/dconvertor/';
+    var targetUrl = 'https://stackoverflow.com/questions/6370690/media-queries-how-to-target-desktop-tablet-and-mobile';
 
 	var request = new XMLHttpRequest();
-	request.open("GET", proxyUrl + "?url=" + encodeURIComponent(targetUrl), true);  // last parameter must be true
-	request.responseType = "document";
-	request.onload = function (e) {
-	  if (request.readyState === 4) {
-		if (request.status === 200) {
-			console.log(request.responseXML);
-		  var a = request.responseXML.querySelector("#app");
-		  console.log(a);
-		} else {
-		  console.error(request.status, request.statusText);
-		}
-	  }
-	};
-	request.onerror = function (e) {
-	  console.error(request.status, request.statusText);
-	};
-	// request.send(null);  // not a POST request, so don't send extra data
+
+	request.onreadystatechange = function() {
+		let jsontext = request.responseText;
+		alert(jsontext);
+	}
+
+	var theLink = '';
+	var myKey = '72fc15d17e984b2f82db06e09121ed44320a66f9fe300007a95379505f9a7a74c650714654fcaa589b5f7462a28f2574f2c4d38eb243872f993df7298c7a8d748bc67959415f0d28c4a0e25e42134848';
+	var myUrl = 'https://justicebringer.github.io/DConvertor/';
+
+	request.open("GET", "https://extraction.import.io/query/extractor" + theLink + '?_apikey=' + myKey + '&url=' + myUrl, true);
+
+	request.send();
+}
+
+window.onload = function (){
 
 }
